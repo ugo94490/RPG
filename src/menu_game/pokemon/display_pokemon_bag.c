473e22a00fa13a25cb3de80f9681b5a0b7dcd0cf
Henkan <hugo.lisik@epitech.eu>
2019-09-29 13:57:03 +0200

    [Fix] fixed norm on pokemon bag function

--- src/menu_game/pokemon/display_pokemon_bag.c
+++ src/menu_game/pokemon/display_pokemon_bag.c
@@ -21,32 +21,36 @@ static void sub_display_slot(pkmn_bag_t *pkmn_bag, int i, sfVector2f temppos)
     sfSprite_setScale(pkmn_bag->slot[i].img.sprite, pkmn_bag->window->scale);
     sfRenderWindow_drawSprite(pkmn_bag->window->window,
     pkmn_bag->slot[i].img.sprite, NULL);
-    pkmn_bag->slot[i].pv.pos.x = temppos.x + (POS_X_SLOT[i] - 243 + 125) * pkmn_bag->window->scale.x;
-    pkmn_bag->slot[i].pv.pos.y = temppos.y + (POS_Y_SLOT[i] - 225 + 75) * pkmn_bag->window->scale.y;
+    pkmn_bag->slot[i].pv.pos.x = temppos.x + (POS_X_SLOT[i] - 243 + 125)
+    * pkmn_bag->window->scale.x;
+    pkmn_bag->slot[i].pv.pos.y = temppos.y + (POS_Y_SLOT[i] - 225 + 75)
+    * pkmn_bag->window->scale.y;
     sfText_setPosition(pkmn_bag->slot[i].pv.text,
     pkmn_bag->slot[i].pv.pos);
-    pkmn_bag->slot[i].m_pv.pos.x = temppos.x + (POS_X_SLOT[i] - 243 + 200) * pkmn_bag->window->scale.x;
-    pkmn_bag->slot[i].m_pv.pos.y = temppos.y + (POS_Y_SLOT[i] - 225 + 75) * pkmn_bag->window->scale.y;
+    pkmn_bag->slot[i].m_pv.pos.x = temppos.x + (POS_X_SLOT[i] - 243 + 200)
+    * pkmn_bag->window->scale.x;
+    pkmn_bag->slot[i].m_pv.pos.y = temppos.y + (POS_Y_SLOT[i] - 225 + 75)
+    * pkmn_bag->window->scale.y;
     sfText_setPosition(pkmn_bag->slot[i].m_pv.text,
     pkmn_bag->slot[i].m_pv.pos);
 }
 
-static void display_slot(pkmn_bag_t *pkmn_bag, game_t *game)
+static void display_slot(pkmn_bag_t *pkmn_bag, game_t *game, sfVector2f temppos)
 {
-    sfVector2f temppos = {(game->character->pos.x + character_width / 2)
-        * game->window->scale.x, (game->character->pos.y +
-        character_height / 6) * game->window->scale.y};
-
     for (int i = 0; i < pkmn_bag->n_pkmn; i += 1) {
         if (pkmn_bag->n_slot == pkmn_bag->slot[i].nbr)
             pkmn_bag->slot[i].rect.left = 0;
         else
             pkmn_bag->slot[i].rect.left = 258;
-        pkmn_bag->slot[i].pos.x = temppos.x + (POS_X_SLOT[i] - 250) * pkmn_bag->window->scale.x;
-        pkmn_bag->slot[i].pos.y = temppos.y + (POS_Y_SLOT[i] - 225) * pkmn_bag->window->scale.y;
+        pkmn_bag->slot[i].pos.x = temppos.x + (POS_X_SLOT[i] - 250)
+        * game->window->scale.x;
+        pkmn_bag->slot[i].pos.y = temppos.y + (POS_Y_SLOT[i] - 225)
+        * pkmn_bag->window->scale.y;
         sub_display_slot(pkmn_bag, i, temppos);
-        sfText_setCharacterSize(pkmn_bag->slot[i].pv.text, 15 * pkmn_bag->window->scale.x);
-        sfText_setCharacterSize(pkmn_bag->slot[i].m_pv.text, 15 * pkmn_bag->window->scale.x);
+        sfText_setCharacterSize(pkmn_bag->slot[i].pv.text, 15
+        * pkmn_bag->window->scale.x);
+        sfText_setCharacterSize(pkmn_bag->slot[i].m_pv.text, 15
+        * pkmn_bag->window->scale.x);
         sfRenderWindow_drawText(pkmn_bag->window->window,
         pkmn_bag->slot[i].pv.text, NULL);
         sfRenderWindow_drawText(pkmn_bag->window->window,
@@ -67,5 +71,5 @@ void display_pokemon_bag(pkmn_bag_t *pkmn_bag, game_t *game)
     sfSprite_setPosition(pkmn_bag->img.sprite, pkmn_bag->pos);
     sfRenderWindow_drawSprite(pkmn_bag->window->window,
     pkmn_bag->img.sprite, NULL);
-    display_slot(pkmn_bag, game);
+    display_slot(pkmn_bag, game, temppos);
 }
